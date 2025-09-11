0a197952a111a6ba3945022244c376809879c84d
Baptiste Zorzi <baptiste@mbz-dev.com>
2025-09-11 15:09:33 +0200

    Adding command to add simple message + list current files

--- lua/compose-ai-message.lua
+++ lua/compose-ai-message.lua
@@ -9,6 +9,8 @@ M.config = {
   mappings = {
     reset = '<leader>mr',
     open_file = '<leader>mo',
+    add_message = '<leader>mm',
+    add_file_paths = '<leader>m.',
     add_visual_selection = '<leader>ma',
     add_full_buffer = '<leader>ma',
     copy_file = '<leader>my',
@@ -109,9 +111,42 @@ function M.add_visual_selection()
   local lang = vim.bo[bufnr].filetype or ""
   local relname = relativename(bufnr)
   local lines = select(1, get_visual_selection())
-  prompt_user("Describe the visual selection:", function(user_message)
+  prompt_user("Describe the visual selection: ", function(user_message)
     add_entry(user_message, relname, lang, lines)
   end)
+end
+
+function M.add_message()
+  local relname = relativename(bufnr)
+  prompt_user("Message : ", function(user_message)
+    local entry = "(" .. relative_path .. ")\n" .. user_message
+
+    local ok = append_file(get_md_file_path(), entry)
+    if ok then
+      vim.notify("Selection added to AI message file.")
+    end
+  end)
+end
+
+function M.add_file_paths()
+  -- Adds all the file paths of all open buffers to the AI message file
+  local bufnrs = vim.api.nvim_list_bufs()
+  local entries = {}
+  for _, bufnr in ipairs(bufnrs) do
+    -- Checking if the buffer is a regular file
+    local listed = vim.api.nvim_get_option_value('buflisted', { buf = bufnr })
+    local path = relativename(bufnr)
+    if listed and path ~= "" then
+      table.insert(entries, '- ' .. path)
+    end
+  end
+  local entry = 'Here are the files I\'m working on:\n'
+    .. table.concat(entries, "\n")
+
+  local ok = append_file(get_md_file_path(), entry)
+  if ok then
+    vim.notify("File paths added to AI message file.")
+  end
 end
 
 function M.add_full_buffer()
@@ -119,7 +154,7 @@ function M.add_full_buffer()
   local lang = vim.bo[bufnr].filetype or ""
   local relname = relativename(bufnr)
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  prompt_user("Describe the full buffer:", function(user_message)
+  prompt_user("Describe the full buffer: ", function(user_message)
     add_entry(user_message, relname, lang, lines)
   end)
 end
@@ -154,6 +189,16 @@ function M.setup_mappings()
   vim.api.nvim_set_keymap("x",
     M.config.mappings.add_visual_selection,
     "<Esc><Cmd>lua require('compose-ai-message').add_visual_selection()<CR>",
+    opts)
+
+  vim.api.nvim_set_keymap("n",
+    M.config.mappings.add_message,
+    "<Esc><Cmd>lua require('compose-ai-message').add_message()<CR>",
+    opts)
+
+  vim.api.nvim_set_keymap("n",
+    M.config.mappings.add_file_paths,
+    "<Esc><Cmd>lua require('compose-ai-message').add_file_paths()<CR>",
     opts)
 
   vim.api.nvim_set_keymap("n",
